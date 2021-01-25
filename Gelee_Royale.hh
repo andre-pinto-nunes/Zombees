75a32a5ab35b59bbca710debbc5da4066f0e4108
dreboy30 <58053186+dreboy30@users.noreply.github.com>
2021-01-25 20:45:12 +0100

    Update Gelee_Royale.hh

--- Gelee_Royale.hh
+++ Gelee_Royale.hh
@@ -4,7 +4,6 @@
 
 class Gelee_Royale : public Projectile {
 	public:
-		// la gelee fait un total de ~50 dmg car elle traverse l'ennemi et rentre en colision plusieures fois
 		Gelee_Royale(int x, int y);
 		~Gelee_Royale(){};
 
